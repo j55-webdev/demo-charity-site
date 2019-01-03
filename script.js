cb3e57cec414b253180361bf88227304121d5d69
j55-webdev <46341803+j55-webdev@users.noreply.github.com>
2019-01-03 12:47:58 +0200

    Create index.html for my-project Github Pages

---
+++ script.js
@@ -0,0 +1,120 @@
+var open2 = document.getElementById('learnMore_2');
+open2.onclick = function(){
+    $('#extraInfo_2').css('display', 'unset');
+    $('#learnMore_2').css('display', 'none');
+    return;
+}
+var close2 = document.getElementById('close_2');
+close2.onclick = function(){
+    $('#extraInfo_2').css('display', 'none');
+    $('#learnMore_2').css('display', 'unset');
+    return;
+}
+
+var open3 = document.getElementById('learnMore_3');
+open3.onclick = function(){
+    
+    $('#extraInfo_3').css('display', 'unset');
+    $('#learnMore_3').css('display', 'none');
+  
+    return;
+}
+var close3 = document.getElementById('close_3');
+close3.onclick = function(){
+    var content3 = document.getElementById('extraInfo_3');
+    content3.style.display = 'none';
+    open3.style.display = 'unset';
+    return;
+}
+
+var open4 = document.getElementById('learnMore_4');
+open4.onclick = function(){
+    var content4 = document.getElementById('extraInfo_4');
+    content4.style.display = 'unset';
+    open4.style.display = 'none';
+    return;
+}
+var close4 = document.getElementById('close_4');
+close4.onclick = function(){
+    var content4 = document.getElementById('extraInfo_4');
+    content4.style.display = 'none';
+    open4.style.display = 'unset';
+    return;
+}
+
+var open5 = document.getElementById('learnMore_5');
+open5.onclick = function(){
+    var content5 = document.getElementById('extraInfo_5');
+    content5.style.display = 'unset';
+    open5.style.display = 'none';
+    return;
+}
+var close5 = document.getElementById('close_5');
+close5.onclick = function(){
+    var content5 = document.getElementById('extraInfo_5');
+    content5.style.display = 'none';
+    open5.style.display = 'unset';
+    return;
+}
+
+var open6 = document.getElementById('learnMore_6');
+open6.onclick = function(){
+    var content6 = document.getElementById('extraInfo_6');
+    content6.style.display = 'unset';
+    open6.style.display = 'none';
+    return;
+}
+var close6 = document.getElementById('close_6');
+close6.onclick = function(){
+    var content6 = document.getElementById('extraInfo_6');
+    content6.style.display = 'none';
+    open6.style.display = 'unset';
+    return;
+}
+
+var open7 = document.getElementById('learnMore_7');
+open7.onclick = function(){
+    var content7 = document.getElementById('extraInfo_7');
+    content7.style.display = 'unset';
+    open7.style.display = 'none';
+    return;
+}
+var close7 = document.getElementById('close_7');
+close7.onclick = function(){
+    var content7 = document.getElementById('extraInfo_7');
+    content7.style.display = 'none';
+    open7.style.display = 'unset';
+    return;
+}
+
+
+
+var open11 = document.getElementById('learnMore_11');
+open11.onclick = function(){
+    var content11 = document.getElementById('extraInfo_11');
+    content11.style.display = 'unset';
+    open11.style.display = 'none';
+    return;
+}
+var close11 = document.getElementById('close_11');
+close11.onclick = function(){
+    var content11 = document.getElementById('extraInfo_11');
+    content11.style.display = 'none';
+    open11.style.display = 'unset';
+    return;
+}
+
+var open12 = document.getElementById('learnMore_12');
+open12.onclick = function(){
+    var content12 = document.getElementById('extraInfo_12');
+    content12.style.display = 'unset';
+    open12.style.display = 'none';
+    return;
+}
+var close12 = document.getElementById('close_12');
+close12.onclick = function(){
+    var content12 = document.getElementById('extraInfo_12');
+    content12.style.display = 'none';
+    open12.style.display = 'unset';
+    return;
+}
